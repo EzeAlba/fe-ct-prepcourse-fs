7b17374c1dcee8a2073165a70a70349c9fe56277
EzeAlba <ezealba2@gmail.com>
2023-07-04 20:47:59 -0300

    modulo 4 terminado

---
+++ prueba.js
@@ -0,0 +1,11 @@
+function esPrimo(num){
+
+    
+  // Ejemplo de uso
+  console.log(esPrimo(2)); // Output: true
+  console.log(esPrimo(7)); // Output: true
+  console.log(esPrimo(10)); // Output: false
+  console.log(esPrimo(17)); // Output: true
+  console.log(esPrimo(1)); // Output: false
+  console.log(esPrimo(-5)); // Output: false
+  
